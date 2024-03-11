a37cbe0f4d2fc12b8f909fdd6edd11f776f09d53
bthanhnhut <bthanhnhut10@gmail.com>
2024-03-12 01:51:02 +0700

    a

--- src/accounts/dto/registerAccount.dto.ts
+++ src/accounts/dto/registerAccount.dto.ts
@@ -1,18 +1,19 @@
-import { IsNotEmpty, IsNumberString, IsInt } from 'class-validator';
+import { IsNotEmpty, IsNumberString, IsInt, IsString } from 'class-validator';
 
 export class RegisterAccount {
   @IsNotEmpty()
+  @IsString()
   username: string;
-
+  @IsString()
   @IsNotEmpty()
   password: string;
-
+  @IsString()
   @IsNotEmpty()
   customer_name: string;
-
+  @IsString()
   @IsNumberString()
   phone_number: string;
-
+  @IsString()
   address: string;
 
   @IsInt()
