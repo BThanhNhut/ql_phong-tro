acce8ed4c0ab66dad7035affe499d4149c298d79
bthanhnhut <bthanhnhut10@gmail.com>
2024-05-14 22:40:06 +0700

    thêm

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -5,9 +5,11 @@ import { JwtModule } from '@nestjs/jwt';
 import { Accounts } from 'src/accounts/accounts.entity';
 import { ImagesController } from './images.controller';
 import { ImagesService } from './images.service';
+import { Images } from './images.entity';
+import { Rooms } from 'src/rooms/rooms.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Accounts, Roles])],
+  imports: [TypeOrmModule.forFeature([Accounts, Roles, Images, Rooms])],
   controllers: [ImagesController],
   providers: [ImagesService],
 })
